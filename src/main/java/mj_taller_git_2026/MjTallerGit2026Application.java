cd56f361e20780ee55e84cb300d1948a386ea278
mj-Vanzfranhait <matiazjara@gmail.com>
2026-09-17 10:21:19 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/mj_taller_git_2026/MjTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package mj_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjTallerGit2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/mj_taller_git_2026/MjTallerGit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package mj_taller_git_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjTallerGit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
